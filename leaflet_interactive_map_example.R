e9d9afbdfbba9e41c1a85d5fc4ea6cc7a6a9707f
Emily Beckman <ebeckman@mortonarb.org>
2020-01-10 12:55:50 -0600

    explainations for users

--- leaflet_interactive_map_example.R
+++ leaflet_interactive_map_example.R
@@ -1,19 +1,32 @@
 ### Author: Emily Beckman  ###  Date: 1/7/2020                                |
 
 ### DESCRIPTION:
-  #
+  # This script creates interactive HTML maps using the 'leaflet' package
+  #   in R. With a basic understanding of leaflet functions, the scripts for
+  #   these basic maps can be easily adjusted for different uses. DataCamp
+  #   has a beginner course for leaflet:
+  #   https://www.datacamp.com/courses/interactive-maps-with-leaflet-in-r
 
 ### INPUTS:
-  #
+  # Any files that can be viewed spatially: points (CSV with lat-long),
+  #   polygons (shapefiles), images (rasters)
+  # This script uses distribution points (CSV with lat-long columns) from
+  #   the Conservation Gap Analysis of Native U.S. Oaks, 2019:
+  #   "all_occ_compiled_unq_all_exsitu_short.csv", which can be downloaded
+  #   here, with appropriate access permissions:
+  #   https://drive.google.com/open?id=1hVJIdO49eZg_ggJnsXeLFkGGnxMWl-Oy
 
 ### OUTPUTS:
-  #
+  # HTML file. You view the map online at a temp address that cannot be
+  #   viewed outside the desktop where you ran the script. File needs to be
+  #   hosted at your own domain for others to view the map also.
 
 
 #################
 ### LIBRARIES ###
 #################
 
+# be sure you've installed all these libraries before trying to add them!!!
 library(leaflet)
 library(RColorBrewer)
 library(tidyverse)
@@ -25,135 +38,108 @@ library(googledrive)
 ### SCRIPT ###
 ##############
 
+# set working directory to location where you have/will put your spatial files
 setwd("./Desktop")
 
-# read in CSV of distribution points
-df <- read.csv("all_occ_compiled_unq_all_exsitu_AJOENSIS.csv")
+#################################
+# 1. Map with toggles for species
+#################################
 
-# take a look at options for color schemes, if needed
-#display.brewer.all()
+# you can view the final product of the following code block hhere:
+#   https://leopardshark.com/esb/oak_test_map.html
+
+# download CSV of oak distribution points (you need permission) and place
+#   the file in the working directory you specified above:
+#   https://drive.google.com/open?id=1hVJIdO49eZg_ggJnsXeLFkGGnxMWl-Oy
+
+# read in CSV of distribution points
+df <- read.csv("all_occ_compiled_unq_all_exsitu_short.csv",as.is=T)
+  str(df) # take a look at the data structure
+
+# reorder data so the ex situ points are at the top (easier to view in map)
+df <- setorder(df, -dataset)
+
+# create list of unique species names; this is the grouping used to toggle
+sp <- unique(df$species)
+
+  # example for creating a grouping variable for number intervals, rather than
+  #   factors like species name
+  #quakes <- quakes %>%
+  #  mutate(groups = case_when(
+  #    stations < 30 ~ 1,
+  #    stations < 50 ~ 2,
+  #    TRUE ~ 3
+  #  ))
 
 # create color scheme
-  # this one is for categorical (factor) variables
+# take a look at options for color pre-made schemes, if needed, by running
+#   "display.brewer.all()"
+# this scheme is for categorical (factor) variables
 palette_fix <- colorFactor(palette = c("#2cb42c","purple"),
                        levels = c("exsitu","insitu"))
-  # this one is for continuous (numeric) variables
+# this scheme is for continuous (numeric) variables
 #palette_cont <- colorNumeric(palette = "RdBu",
 #                             domain = c(1:20))
 
-# take a look at options for basemaps ("provider tiles")
-#names(providers)
+# write text that will be map's title
+title <- "In situ distribution and ex situ source localities for native U.S.
+  oaks of conservation concern"
 
-# create a map
-map <- df %>%
-  leaflet() %>%
-  # you can add as many separate provider tiles as you wish:
+# create function to plot the map
+map_layers <- function(){
+  map <- leaflet() %>%
+  # add base map; you can add as many separate provider tiles as you wish
+  # see all provider tiles (base maps) by running "names(providers)"
   addProviderTiles(
     "CartoDB.PositronNoLabels",
-    options = providerTileOptions(maxZoom = 9),
-    group = "Base map") %>%
-  # the "popup" is the text that appears when you click a point
-  addCircleMarkers(
-    lng = ~longitude, lat = ~latitude,
-    popup = ~paste0("<b>","<i>",species,"</b>","</i>","<br/>",dataset),
-    color = ~palette_fix(dataset), #"#2cb42c"
-    radius = 3,
-    group = "Q. ajoensis"
-  ) %>%
-  # this creates a little box where you can click layers on and off
-  addLayersControl(
-    baseGroups = c("Base map"),
-    overlayGroups = c("Q. ajoensis"),
-    options = layersControlOptions(collapsed = F)
-  ) %>%
-  # something isn't quite right here
-  addLegend(
-    labels = c("Ex situ","In situ"),
-    colors = ~palette_fix(dataset),
-    title = "Legend",
-    position = "topright",
-    opacity = 0.75)
-# view your map!
-map
-
-### Map with toggles for species ###
-
-# load data
-df <- read.csv("all_occ_compiled_unq_all_exsitu_short.csv",as.is=T)
-  str(df)
-
-# order data so ex situ points are at the top
-df <- setorder(df, -dataset)
-  str(df)
-
-# create list of unique species names (grouping to use for toggle)
-sp <- unique(df$species)
-
-# create title text
-title <- "In situ distribution and ex situ source localities for native U.S. oaks of conservation concern"
-
-# Example for creating a grouping variable for number intervals
-#quakes <- quakes %>%
-#  mutate(groups = case_when(
-#    stations < 30 ~ 1,
-#    stations < 50 ~ 2,
-#    TRUE ~ 3
-#  ))
-
-# function to plot a map with layer selection
-map_layers <- function(){
-  # base map
-  map <- leaflet() %>%
-    addProviderTiles(
-      "CartoDB.PositronNoLabels",
-      options = providerTileOptions(maxZoom = 10))
-  # loop through all groups and add a layer one at a time
+    # you can change the "maxZoom" option depending on how far you want
+    #   viewers to be able to zoom (addresses privacy issues)
+    options = providerTileOptions(maxZoom = 10))
+  # loop through all groups and add each toggle layer one at a time
   for (i in 1:length(sp)) {
     map <- map %>%
-      addCircleMarkers(
-        data = df %>%
-          filter(species == sp[i]),
-          group = sp[i],
-          lng = ~longitude, lat = ~latitude,
-          radius = 4,
-          color = ~palette_fix(dataset),
-          fillOpacity = 1,
-          stroke = F
-      )
+    addCircleMarkers(
+      data = df %>%
+        # change 'species' to the name of the column you groupped
+        filter(species == sp[i]),
+        group = sp[i],
+        lng = ~longitude, lat = ~latitude,
+        radius = 4,
+        # change 'dataset' to the name of the column you want to define
+        #   the color of the points
+        color = ~palette_fix(dataset),
+        fillOpacity = 1,
+        stroke = F
+    )
   }
-  # create layer control
+  # add layer control
   map %>%
-    # add title
-    addControl(title, position = "topright") %>%
-    # add legend
-    addLegend(
-      labels = c("Yes","No"),
-      colors = c("#2cb42c","purple"),
-      title = "Represented ex situ?",
-      position = "topright",
-      opacity = 0.75) %>%
-    # add layer control panel
-    addLayersControl(
-      overlayGroups = sp,
-      options = layersControlOptions(collapsed = F)) %>%
-    hideGroup(sp[2:length(sp)]) # hide all groups except the 1st one
+  # add title
+  addControl(title, position = "topright") %>%
+  # add legend
+  addLegend(
+    labels = c("Yes","No"),
+    colors = c("#2cb42c","purple"),
+    title = "Represented ex situ?",
+    position = "topright",
+    opacity = 0.75) %>%
+  # add layer control panel
+  addLayersControl(
+    overlayGroups = sp,
+    options = layersControlOptions(collapsed = F)) %>%
+  hideGroup(sp[2:length(sp)]) # hide all groups except the 1st one
 }
 
 # view map
-map <- map_layers()
+map_layers()
 
-### TESTING how to save map
-htmlwidgets::saveWidget(map, file = "oak_test_map.html")
+# save map
+map_final <- map_layers()
+htmlwidgets::saveWidget(map_final, file = "oak_test_map.html")
 
-drive_upload(htmlwidgets::saveWidget(map, file = "oak_test_map.html"))
-
-htmlwidgets::saveWidget(map, file = "oak_test_map.html")
-mapshot(map, url = paste0(getwd(),"/oak_test_map.html"))
-
-
-
-
-### EXAMPLE used to create oak map ###
+####################################################
+# 2. Original exmaple used to create toggle oak map
+####################################################
 
 # load data
 data("quakes")
@@ -195,12 +181,41 @@ map_layers <- function(){
 # view map
 map_layers()
 
+#############################################
+# 3. Personal test of fixed map (no toggles)
+#############################################
 
+# read in CSV of distribution points
+df <- read.csv("all_occ_compiled_unq_all_exsitu_AJOENSIS.csv")
 
-# how to remove things from map
-map2 <- map %>%
-  clearBounds() %>%
-  clearMarkers()
-
-# <br/> = line break
-# <i></i> = italics
+# create a map
+map <- df %>%
+  leaflet() %>%
+  # you can add as many separate provider tiles as you wish:
+  addProviderTiles(
+    "CartoDB.PositronNoLabels",
+    options = providerTileOptions(maxZoom = 9),
+    group = "Base map") %>%
+  # the "popup" is the text that appears when you click a point
+  addCircleMarkers(
+    lng = ~longitude, lat = ~latitude,
+    popup = ~paste0("<b>","<i>",species,"</b>","</i>","<br/>",dataset),
+    color = ~palette_fix(dataset), #"#2cb42c"
+    radius = 3,
+    group = "Q. ajoensis"
+  ) %>%
+  # this creates a little box where you can click layers on and off
+  addLayersControl(
+    baseGroups = c("Base map"),
+    overlayGroups = c("Q. ajoensis"),
+    options = layersControlOptions(collapsed = F)
+  ) %>%
+  # something isn't quite right here
+  addLegend(
+    labels = c("Ex situ","In situ"),
+    colors = ~palette_fix(dataset),
+    title = "Legend",
+    position = "topright",
+    opacity = 0.75)
+# view your map!
+map
