66b7cc894e40c600950d39a369f0ed474e3e5d83
Emily Beckman <ebeckman@mortonarb.org>
2020-11-18 08:50:08 -0600

    Alphabatize comma-separated elements in a cell

--- scripts/tidy_data.R
+++ scripts/tidy_data.R
@@ -1,6 +1,11 @@
 
 # load tidyverse library with packages we'll use
 library(tidyverse)
+library(data.table)
+
+##
+### We want one row for each observation (rather than concatenated in a cell)
+##
 
 # read in data
 data <- read.csv("./Desktop/work/Oak spp. with threat code and country.csv",
@@ -22,4 +27,34 @@ str(data2)
 
 # write file
 write.csv(data2, "./Desktop/work/Oak spp. with threat code and country - tidy.csv",
+  row.names = F)
+
+##
+### We want to arrange the countries in one cell alphabetically
+##
+
+# read in data
+data <- read.csv("./Desktop/work/Appendices - Quercus species - Alphabetically.csv",
+  stringsAsFactors = F)
+# glance at data
+str(data)
+
+# make sure there aren't any spaces in the country codes column
+data$Country.distribution <- gsub(" ", "", data$Country.distribution)
+sort(unique(data$Country.distribution))
+
+# separate country distribution column by comma then arrange each element
+#   alphabetically, then join back to data
+ctry_arranged <- setDT(data)[,list(Country.distribution =
+  toString(sort(unique(strsplit(Country.distribution,',')[[1]])))),
+  by = Genus.species]
+data <- data %>%
+  dplyr::select(-Country.distribution) %>%
+  full_join(ctry_arranged)
+# remove spaces in the new column
+data$Country.distribution <- gsub(" ", "", data$Country.distribution)
+unique(data$Country.distribution)
+
+# write file
+write.csv(data, "./Desktop/work/Appendices - Quercus species - Alphabetically - tidy.csv",
   row.names = F)
